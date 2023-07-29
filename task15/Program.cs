c84ad918d8f6ab90bfba87f7654a968fffcc5f1c
yershova-margarita <yershova-margarita@yandex.ru>
2023-07-29 20:56:55 +0300

    Добавлено решение задачи 15

---
+++ task15/Program.cs
@@ -0,0 +1,33 @@
+﻿/* Напишите программу, которая принимает на вход цифру, 
+обозначающую день недели, и проверяет, 
+является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет
+*/
+bool ValidayEnterNum(int num)
+{
+    if (num > 0 && num < 8)
+    {
+        return true;
+    }
+    return false;
+}
+
+bool IsWeekend(int number)
+{
+    if (number == 6 || number == 7)
+    {
+        return true;
+    }
+    return false;
+}
+
+Console.WriteLine($"Введите число, соответствующее дню недели от 1 до 7");
+int userNum = Convert.ToInt32(Console.ReadLine());
+if (ValidayEnterNum(userNum)) 
+{
+    if (IsWeekend(userNum))  Console.WriteLine("Выходной день");
+    else Console.WriteLine("Рабочий день");
+}
+else Console.WriteLine("Введенное число не соответствует дням недели");
