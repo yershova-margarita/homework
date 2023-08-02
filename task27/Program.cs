88d0e6642e4c68dc982e33ef34f48268952b4b70
yershova-margarita <yershova-margarita@yandex.ru>
2023-08-02 16:08:18 +0300

    Добавлено решение задачи 27

---
+++ task27/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+Напишите программу, которая принимает на вход число 
+и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+int SumDigit(int num)
+{
+    int sum = 0;
+    while (num > 0)
+    {
+        sum += num % 10;
+        num = num / 10;
+    }
+    return sum;
+}
+
+bool ValidateNum(int num)
+{
+    if (num < 0)
+    {
+        Console.WriteLine($"Число {num} не является натуральным, повторите ввод");
+        return false;
+    }
+    else return true;
+}
+
+Console.WriteLine("Введите число :");
+int userNum = Convert.ToInt32(Console.ReadLine());
+
+if (ValidateNum(userNum)) Console.WriteLine($"Сумма цифр числа {userNum} : {SumDigit(userNum)}");
