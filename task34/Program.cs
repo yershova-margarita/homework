e52fb89878d348ede2b024ca6af25629bc2059bb
yershova-margarita <yershova-margarita@yandex.ru>
2023-08-10 09:23:08 +0300

    Добавлено решение задачи 34 на нахождение числа положительных трехзначных элементов в массиве

---
+++ task34/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2
+*/
+
+int[] CreateArrayRndInt(int size)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(100, 1000);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
+
+int[] array = CreateArrayRndInt(4);
+PrintArray(array);
+
+int result = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] % 2 == 0)
+    {
+        result++;
+    }
+}
+Console.WriteLine();
+Console.WriteLine($"В массиве {result} четных чисел");
