497facec4576eee385451588c0ee4cf19e34a64f
yershova-margarita <yershova-margarita@yandex.ru>
2023-08-28 11:12:32 +0300

    fix

--- task43/Program.cs
+++ task43/Program.cs
@@ -4,13 +4,13 @@
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
 
-double [] PointXY(double b1, double k1, double b2, double k2, int size = 2)
+double[] PointXY(double b1, double k1, double b2, double k2, int size = 2)
 {
     double[] array = new double[size];
     double x = (b1 - b2) / (k2 - k1);
     double y = k1 * x + b1;
-    array[0] = x;
-    array[1] = y;
+    array[0] = Math.Round(x, 1);
+    array[1] = Math.Round(y, 1);
     return array;
 }
 Console.WriteLine("Введите b1: ");
@@ -31,5 +31,5 @@ if (userK1 == userK2) Console.WriteLine("Прямые параллельны");
 else
 {
     double[] userArray = PointXY(userB1, userK1, userB2, userK2);
-    Console.WriteLine($"Точки пересечения: {userArray[0]}, {userArray[1]}");
+    Console.WriteLine($"Точка пересечения прямых: ({userArray[0]}, {userArray[1]})");
 }
