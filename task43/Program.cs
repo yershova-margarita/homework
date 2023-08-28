f3770315bf917552e88dbf5216f4843516795c4a
yershova-margarita <yershova-margarita@yandex.ru>
2023-08-28 10:50:41 +0300

    fix

--- task43/Program.cs
+++ task43/Program.cs
@@ -3,41 +3,24 @@
 // значения b1, k1, b2 и k2 задаются пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
-double PointX(int k1, int k2, int b1, int b2)
+Console.WriteLine("Введите b1: ");
+double userB1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите k1: ");
+double userK1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите b2: ");
+double userB2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите k2: ");
+double userK2 = Convert.ToDouble(Console.ReadLine());
+
+if ((userK1 == userK2) && (userB1 == userB2))
 {
-    double x = (-b2 + b1) / (-k1 + k2);
-    return x;
+    Console.WriteLine("Прямые совпадают");
+    return;
 }
-
-double PointY(double x, int k2, int b2)
+if (userK1 == userK2) Console.WriteLine("Прямые параллельны");
+else
 {
-    double[] array = new double[2];
-    double x = (-b2 + b1) / (-k1 + k2);
-    double y = k2 * x + b2;
-    // double x = 0.5;
-    // double y = 0.5;
-    array[0] = Math.Round(x, 1);
-    array[1] = Math.Round(y, 1);
-    return array;
+    double x = (userB1 - userB2) / (userK2 - userK1);
+    double y = userK1 * x + userB1;
+    Console.WriteLine($"Точки пересечения: {x}, {y}");
 }
-
-// Console.WriteLine("Введите b1: ");
-// int userB1 = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Введите k1: ");
-// int userK1 = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Введите b2: ");
-// int userB2 = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Введите k2: ");
-// int userK2 = Convert.ToInt32(Console.ReadLine());
-
-// if ((userK1 == userK2) && (userB1 == userB2))
-// {
-//     Console.WriteLine("Прямые совпадают");
-//     return;
-// }
-// if (userK1 == userK2) Console.WriteLine("Прямые параллельны");
-// else
-// {
-//     Console.WriteLine($"Точка пересечения {PointXY(userK1, userK2, userB1, userB2)}");
-// }
-Console.WriteLine($"Точка пересечения {PointXY(2, 5, 4, 9)}");
