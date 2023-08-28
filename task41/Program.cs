dd21074f32b79649a032db716f3db5233c5978f7
yershova-margarita <yershova-margarita@yandex.ru>
2023-08-28 08:42:37 +0300

    добавлено решение задачи 41

---
+++ task41/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+int[] InputArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"Введите {i + 1} элемент массива: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+int CountPositiveNum(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+Console.WriteLine("Введите количество элементов: ");
+int userNum = Convert.ToInt32(Console.ReadLine());
+
+int[] userArr = InputArray(userNum);
+PrintArray(userArr);
+int userCount = CountPositiveNum(userArr);
+
+Console.WriteLine($"Пользователь ввёл {userCount} чисел больше 0");
