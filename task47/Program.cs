3f8481b6facb1134e147f354f44f97af16e58206
yershova-margarita <yershova-margarita@yandex.ru>
2023-08-29 20:26:50 +0300

    Унифицировано решение задачи 47

--- task47/Program.cs
+++ task47/Program.cs
@@ -5,6 +5,12 @@
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
 
+int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
 double[,] CreateRndMatrixDouble(int rows, int cols, int min, int max)
 {
     double[,] matrix = new double[rows, cols];
@@ -13,7 +19,8 @@ double[,] CreateRndMatrixDouble(int rows, int cols, int min, int max)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            matrix[i, j] = rnd.Next(min, max + 1) / 10.0;
+            matrix[i, j] = min + rnd.NextDouble() * (max - min);
+            matrix[i, j] = Math.Round(matrix[i, j], 1);
         }
     }
     return matrix;
@@ -33,5 +40,10 @@ void PrintMatrix(double[,] matrix)
 	}
 }
 
-double[,] array2d = CreateRndMatrixDouble(3, 4, -100, 100);
+int userRows = ReadInt("Введите количество строк: ");
+int userCols = ReadInt("Введите количество столбцов: ");
+int userMin = ReadInt("Введите минимальное число для заполнения матрицы: ");
+int userMax = ReadInt("Введите максимальное число для заполнения матрицы: ");
+
+double[,] array2d = CreateRndMatrixDouble(userRows, userCols, userMin, userMax);
 PrintMatrix(array2d);
